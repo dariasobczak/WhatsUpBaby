e055653d9b7a3825a997a75d562c68b3d69889e4
Daria Sobczak <daria.a.sobczak@gmail.com>
2016-06-04 11:08:37 +0200

    #usuwam

--- opiekun/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class OpiekunConfig(AppConfig):
-    name = 'opiekun'
--- opiekun/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render_to_response
-
-def loggedin(request):
-    return render_to_response('loggedin.html',
-                              {'full_name': request.user.username})
-
-# Create your views here.
